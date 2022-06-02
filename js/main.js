54fb5362b9ecfceffd2deca34d7eab770f323267
markonser <markonser@yandex.ru>
2022-06-02 11:32:11 +0300

    добавлены функции, возвращающие случайное целое и с плавающей точкой из переданного диапазона включительно

---
+++ js/main.js
@@ -0,0 +1,57 @@
+function getRandomCeilInclusive(min, max) {
+  let temp = Math.random() * (max - min + 1) + min;
+  if (isNaN(min)
+    || isNaN(max)
+    || !Number.isInteger(min)
+    || !Number.isInteger(max)
+    || min === ''
+    || max === ''
+    || max < min
+    ) {
+    return 'Не корректные входные значения';
+  }
+  else if (max === min) {
+    return min;
+  }
+  else if (min >= 0 && max >= min) {
+    temp = Math.random() * (max - min + 1) + min;
+    return Math.floor(temp);
+  }
+  return console.log('Проверьте корректность входного диапазона');
+}
+
+console.log('Ceil',getRandomCeilInclusive(1,10));
+
+
+function getRandomFloatInclusive(min, max, coma) {
+  let temp = Math.random() * (max - min + Math.pow(10, -coma)) + min;
+  if (!Number.isInteger(coma)
+    || coma > 16
+    || isNaN(min)
+    || isNaN(max)
+    || isNaN(coma)
+    || min === ''
+    || max === ''
+    || coma === ''
+    || max < min
+    || coma < 0
+    ) {
+    return 'Не корректные входные значения';
+  }
+  else if (min === 0 && max === min && coma === 0) {
+    return 0;
+  }
+  else if (max === min && coma >= 0) {
+    return min;
+  }
+  else if (min >= 0 && max >= min && coma === 0) {
+    temp = Math.random() * (max - min + 1) + min;
+    return Math.floor(temp);
+  }
+  else if (min >= 0 && max >= min && coma > 0) {
+    return parseFloat(temp.toFixed(coma));
+  }
+  return console.log('Проверьте корректность входного диапазона');
+}
+
+console.log('Float',getRandomFloatInclusive(5.5, 5.5, 5));
