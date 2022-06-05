3da9eb2479c61393cb49b125a40f9818608e8057
markonser <markonser@yandex.ru>
2022-06-05 19:07:09 +0300

    4.15. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,54 +1,95 @@
-function getRandomCeilInclusive(min, max) {
-  let temp = Math.random() * (max - min + 1) + min;
-  if (isNaN(min)
-    || isNaN(max)
-    || !Number.isInteger(min)
-    || !Number.isInteger(max)
-    || min === ''
-    || max === ''
-    || max < min
-  ) {
-    return 'Не корректные входные значения';
-  }
-  else if (max === min) {
-    return min;
-  }
-  else if (min >= 0 && max >= min) {
-    temp = Math.random() * (max - min + 1) + min;
-    return Math.floor(temp);
-  }
-  return 'Проверьте корректность входного диапазона';
+'use strict';
+const AVATARS_IMG = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const FEATURE_OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+const HOUSE_DESCRIPTIONS = [
+  'Комфортное жилье со всем необходимым',
+  'Стильное жилье с удобствами',
+  'Больше, чем место для сна',
+  'Недавно произведен ремонт',
+]
+const HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel']
+const TIME_CHECK_IN_OUTS = ['12:00', '13:00', '14:00']
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+]
+const COORDINATES = {
+  lat: [35.65000, 35.70000],
+  lng: [139.70000, 139.80000]
 }
-getRandomCeilInclusive(1, 3);
-
-function getRandomFloatInclusive(min, max, coma) {
-  let temp = Math.random() * (max - min + Math.pow(10, -coma)) + min;
-  if (!Number.isInteger(coma)
-    || coma > 16
-    || isNaN(min)
-    || isNaN(max)
-    || isNaN(coma)
-    || min === ''
-    || max === ''
-    || coma === ''
-    || max < min
-    || coma < 0
-  ) {
-    return 'Не корректные входные значения';
+const createSuggest = () => {
+  const getAvatarImgUrl = 'img/avatars/user'
+    + `${getRandomPositiveInteger(1, AVATARS_IMG.length - 1) < 10
+      ? '0' + getRandomPositiveInteger(1, AVATARS_IMG.length - 1)
+      : getRandomPositiveInteger(1, AVATARS_IMG.length - 1)}`
+    + '.png';
+  function getRandomPositiveInteger(a, b) {
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+    const result = Math.random() * (upper - lower + 1) + lower;
+    return Math.floor(result);
   }
-  else if (min === 0 && max === min && coma === 0) {
-    return 0;
+  function getRandomPositiveFloat(a, b, digits = 5) {
+    const lower = Math.min(Math.abs(a), Math.abs(b));
+    const upper = Math.max(Math.abs(a), Math.abs(b));
+    const result = Math.random() * (upper - lower) + lower;
+    return +result.toFixed(digits);
   }
-  else if (max === min && coma >= 0) {
-    return min;
+  function getManyOptions(options) {
+    const maxLength = options.length;
+    const lengthOfNewArr = getRandomPositiveInteger(1, maxLength);
+    const array = [];
+    while (array.length < lengthOfNewArr) {
+      const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
+      const el = options[indexOfEl];
+      if (!array.includes(el)) {
+        array.push(el);
+      }
+    }
+    return array;
   }
-  else if (min >= 0 && max >= min && coma === 0) {
-    temp = Math.random() * (max - min + 1) + min;
-    return Math.floor(temp);
+  function getOneOption(options) {
+    const maxLength = options.length;
+    const lengthOfNewArr = 1;
+    const array = [];
+    while (array.length < lengthOfNewArr) {
+      const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
+      const el = options[indexOfEl];
+      array.push(el);
+    }
+    return array;
   }
-  else if (min >= 0 && max >= min && coma > 0) {
-    return parseFloat(temp.toFixed(coma));
+  const getLat = getRandomPositiveFloat(COORDINATES.lat[0], COORDINATES.lat[1]);
+  const getLng = getRandomPositiveFloat(COORDINATES.lng[0], COORDINATES.lng[1]);
+  return {
+    author: {
+      avatar: getAvatarImgUrl
+    },
+    offer: {
+      title: 'Объявление',
+      address: getLat + ',' + getLng,
+      price: getRandomPositiveInteger(100, 999),
+      type: getOneOption(HOUSE_TYPES),
+      rooms: getRandomPositiveInteger(1, 10),
+      guests: getRandomPositiveInteger(1, 10),
+      checkin: getOneOption(TIME_CHECK_IN_OUTS),
+      checkout: getOneOption(TIME_CHECK_IN_OUTS),
+      features: getManyOptions(FEATURE_OPTIONS),
+      description: String(getOneOption(HOUSE_DESCRIPTIONS)),
+      photos: getManyOptions(PHOTOS)
+    },
+    location: {
+      lat: getLat,
+      lng: getLng
+    }
   }
-  return 'Проверьте корректность входного диапазона';
+};
+function generateManySuggestions(count) {
+  const Arr = [];
+  while (Arr.length <= count) {
+    const el = createSuggest();
+    Arr.push(el);
+  }
+  return Arr
 }
-getRandomFloatInclusive(1, 2, 3);
