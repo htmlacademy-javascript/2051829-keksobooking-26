37141cb0ffa332bee37c4c691158e607d1119e81
markonser <markonser@yandex.ru>
2022-06-08 15:52:16 +0300

    после рефакторинга с наставником

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-
-const AVATARS_IMG = {
-  from: 1,
-  to: 10,
-  prefix: '0'
-};
 const FEATURE_OPTIONS = [
   'wifi',
   'dishwasher',
@@ -35,102 +29,68 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
-const COORDINATES = {
+const COORDINATE_RANGES = {
   lat: [35.65000, 35.70000],
   lng: [139.70000, 139.80000]
 };
-const getRandomArrayElement = (elements) => {
-  const element = elements[getRandomPositiveInteger(0, elements.length - 1)];
-  return element;
-};
-const generateAvatarUrls = (from, to, prefix) => {
-  const tmp = [];
-  for (let i = from; i <= to; ++i) {
-    tmp.push(`${'img/avatars/user'}${i < 10 ? prefix + i : i}${'.png'}`);
-  }
-  return tmp;
-};
 
-let avatarUrls = [];
-
-const getAvatar = (a, b, c) => {
-  if (!avatarUrls.length) {
-    avatarUrls = generateAvatarUrls(a, b, c);
-  }
-  const src = getRandomArrayElement(avatarUrls);
-  const index = avatarUrls.indexOf(src);
-  avatarUrls.splice(index, 1);
-  return src;
-};
-function getRandomPositiveInteger(a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+function getRandomInteger(from, to) {
+  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
+  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
-function getRandomPositiveFloat(a, b, digits = 5) {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
+function getRandomFloat(from, to, digits = 5) {
+  const lower = Math.min(Math.abs(from), Math.abs(to));
+  const upper = Math.max(Math.abs(from), Math.abs(to));
   const result = Math.random() * (upper - lower) + lower;
   return +result.toFixed(digits);
 }
-function getManyOptions(options) {
-  const maxLength = options.length;
-  const lengthOfNewArr = getRandomPositiveInteger(1, maxLength);
-  const array = [];
-  while (array.length < lengthOfNewArr) {
-    const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
-    const el = options[indexOfEl];
-    if (!array.includes(el)) {
-      array.push(el);
+function getRandomArrayElements(arr) {
+  const size = getRandomInteger(0, arr.length);
+  const result = [];
+  while (result.length < size) {
+    const el = getRandomInteger(0, size - 1);
+    if (!result.includes(el)) {
+      result.push(el);
     }
   }
-  return array;
+  return result;
 }
-function getOneOption(options) {
-  const maxLength = options.length;
-  const lengthOfNewArr = 1;
-  const array = [];
-  while (array.length < lengthOfNewArr) {
-    const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
-    const el = options[indexOfEl];
-    array.push(el);
-  }
-  return array;
-}
-const newOffer = () => {
-  const createOffer = {
+
+const createOffer = (index) => {
+  const lat = getRandomFloat(COORDINATE_RANGES.lat[0], COORDINATE_RANGES.lat[1]);
+  const lng = getRandomFloat(COORDINATE_RANGES.lng[0], COORDINATE_RANGES.lng[1]);
+  const checkInTime = TIME_CHECK_IN_OUTS[getRandomInteger(0, TIME_CHECK_IN_OUTS.length - 1)];
+  return {
     author: {
-      avatar: getAvatar(AVATARS_IMG.from, AVATARS_IMG.to, AVATARS_IMG.prefix)
+      avatar: `${'img/avatars/user'}${index < 10 ? 0 + index : index}${'.png'}`
     },
     offer: {
       title: 'Объявление',
-      address: '',
-      price: getRandomPositiveInteger(100, 999),
-      type: getOneOption(HOUSE_TYPES),
-      rooms: getRandomPositiveInteger(1, 10),
-      guests: getRandomPositiveInteger(1, 10),
-      checkin: getOneOption(TIME_CHECK_IN_OUTS),
-      checkout: getOneOption(TIME_CHECK_IN_OUTS),
-      features: getManyOptions(FEATURE_OPTIONS),
-      description: String(getOneOption(HOUSE_DESCRIPTIONS)),
-      photos: getManyOptions(PHOTOS)
+      address: `${lat}, ${lng}`,
+      price: getRandomInteger(100, 999),
+      type: HOUSE_TYPES[getRandomInteger(0, HOUSE_TYPES.length - 1)],
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 10),
+      checkin: checkInTime,
+      checkout: checkInTime,
+      features: getRandomArrayElements(FEATURE_OPTIONS),
+      description: HOUSE_DESCRIPTIONS[getRandomInteger(0, HOUSE_DESCRIPTIONS.length - 1)],
+      photos: getRandomArrayElements(PHOTOS)
     },
     location: {
-      lat: getRandomPositiveFloat(COORDINATES.lat[0], COORDINATES.lat[1]),
-      lng: getRandomPositiveFloat(COORDINATES.lng[0], COORDINATES.lng[1])
+      lat: lat,
+      lng: lng
     }
   };
-  createOffer.offer.address = `${createOffer.location.lat}, ${createOffer.location.lng}`;
-  return createOffer;
 };
 function generateOffers(count) {
-  const Arr = [];
-  while (Arr.length < count) {
-    const el = newOffer();
-    Arr.push(el);
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(createOffer(i));
   }
-  return Arr;
+  return result;
 }
 generateOffers(10);
 // console.log(generateOffers(10));
