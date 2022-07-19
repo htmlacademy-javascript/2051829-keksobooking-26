4d85d807b8c0eda7678d4d3feeaafab2eb9a6b41
markonser <markonser@yandex.ru>
2022-07-19 17:32:24 +0300

    fix 7

--- js/modules/add-form-validation.js
+++ js/modules/add-form-validation.js
@@ -112,7 +112,7 @@ const onUserFormSubmit = (evt) => {
 };
 
 const activateFormValidation = () => {
-  pristine.addValidator(titleInputElement, validateTitle, getTitleError);
+  pristine.addValidator(titleInputElement, validateTitle, getTitleError,2,true);
   pristine.addValidator(capacitySelectElement, validateRooms, getRoomsErrorMessage);
   pristine.addValidator(pricePerNightInputElement, validatePriceOfType, getPriceErrorMessage);
   pristine.addValidator(addressInputElement, validateAddress, 'Обязательное поле');
